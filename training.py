b08ff2a50dbf1b78a592dae7f7a6020582e715fd
mhanifrmd <46512664+mhanifrmd@users.noreply.github.com>
2019-01-09 14:06:46 +0700

    Delete training.py

--- training.py
+++
@@ -1,8 +0,0 @@
-
-dictionary = {'car':'mobil', 'motorcycle':'sepeda motor', 'cat':'kucing', 'dog':'anjing'}
-
-a = input('masukan kata:')
-
-for k,v in dictionary.items():
-	if a.lower()==k:
-		print(v)
